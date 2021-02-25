4460a28eac8e591a8205ba2e43202b90a328a18b
NamanJain6499 <na.jain660@gmail.com>
2021-02-25 12:41:37 +0530

    first commit

--- src/InterviewBitPractice/LinkList/RemoveDuplicatesfromSortedList.java
+++ src/InterviewBitPractice/LinkList/RemoveDuplicatesfromSortedList.java
@@ -1,13 +1,11 @@
 package InterviewBitPractice.LinkList;
-//solution
-//https://www.youtube.com/watch?v=R6-PnHODewY
+
 public class RemoveDuplicatesfromSortedList {
     public static void main(String args[]){
-        RemoveDuplicatesfromSortedList remove=new RemoveDuplicatesfromSortedList();
-        //1->2->3->3->4->4->5
+        RemoveDuplicatesfromSortedList list=new RemoveDuplicatesfromSortedList();
         ListNode one=new ListNode(1);
-        ListNode two=new ListNode(2);
-        ListNode three=new ListNode(3);
+        ListNode two=new ListNode(1);
+        ListNode three=new ListNode(1);
         ListNode four =new ListNode(3);
         ListNode five=new ListNode(4);
         ListNode six=new ListNode(4);
@@ -18,34 +16,28 @@ public class RemoveDuplicatesfromSortedList {
         four.next=five;
         five.next=six;
         six.next=seven;
-        ListNode answer=RemoveDuplicatesfromSortedList.deleteDuplicates(one);
+        ListNode answer=list.deleteDuplicates(one);
         System.out.println(answer.val);
     }
 
-    public static ListNode deleteDuplicates(ListNode head) {
-        ListNode dummy=new ListNode(0);
-        dummy.next=head;
-        ListNode previous=dummy;
-
-        while (head!=null ){
-            if (head.next!=null && head.val== head.next.val){
-                //skip nodes whose value equal to head
-                while (head.next!=null && head.val== head.next.val){
-                    head=head.next;
-                }
-                previous.next=head.next;
-            }else
-                previous=previous.next;
-            head=head.next;
+    private ListNode deleteDuplicates(ListNode head) {
+        ListNode temp=head;
+        while (temp!=null &&temp.next!=null){
+            if (temp.val== temp.next.val){
+                temp.next=temp.next.next;
+            }
+            else {x`
+                temp=temp.next;
+            }
         }
-        return dummy.next;
+        return head;
     }
 
-   public static class ListNode {
+
+    public static class ListNode {
         public int val;
         public ListNode next;
         ListNode(int x) { val = x; next = null; }
     }
-
 
 }
