a9a3cd5f5548c28c38e80df668b2876ab5c25537
NamanJain6499 <na.jain660@gmail.com>
2021-03-14 13:11:35 +0530

    first commit

---
+++ src/InterviewBitPractice/ArrayList/Print1to100WithoutNumber.java
@@ -0,0 +1,11 @@
+package InterviewBitPractice.ArrayList;
+
+public class Print1to100WithoutNumber {
+    public static void main(String args[]){
+        int one='A'/'A';
+        String ten="..........";
+        for (int i=one;i<=ten.length()*ten.length();i++){
+            System.out.println(i);
+        }
+    }
+}
