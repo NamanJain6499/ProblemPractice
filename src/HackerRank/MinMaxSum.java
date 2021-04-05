97b34d8557af58bbe938aa9413adddd7b63cfab5
NamanJain6499 <na.jain660@gmail.com>
2021-04-05 14:40:45 +0530

    first commit

---
+++ src/HackerRank/MinMaxSum.java
@@ -0,0 +1,44 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class MinMaxSum {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        long min=arr[0];
+        long max=min;
+        long totalSum=arr[0];
+        for(int i=1;i<5;i++){
+            totalSum+=arr[i];
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        long minSum=totalSum-max;
+        long maxSum=totalSum-min;
+        System.out.println(minSum+" "+maxSum);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
