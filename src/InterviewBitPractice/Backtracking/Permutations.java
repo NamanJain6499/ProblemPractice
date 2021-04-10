e20785fd7c68aad4cada1ebe8a6fc7409275ad90
NamanJain6499 <na.jain660@gmail.com>
2021-04-10 18:05:55 +0530

    first commit

--- src/InterviewBitPractice/Backtracking/Permutations.java
+++ src/InterviewBitPractice/Backtracking/Permutations.java
@@ -2,6 +2,8 @@ package InterviewBitPractice.Backtracking;
 
 import java.util.ArrayList;
 
+import static java.util.Collections.swap;
+
 public class Permutations {
     public static void main(String args[]) {
         Permutations permutations = new Permutations();
@@ -13,8 +15,27 @@ public class Permutations {
         System.out.println(answer);
     }
 
-    private ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> a) {
-        return null;
+    private ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> A) {
+        ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>();
+        permuteUntil(A,0,A.size(),result);
+        return result;
+    }
+
+    private void permuteUntil(ArrayList<Integer> A, int left, int right, ArrayList<ArrayList<Integer>> result) {
+        if (left==right){
+            result.add(new ArrayList<>(A));
+            return;
+        }
+        for (int i=left;i<right;i++){
+            swap(A,left,i);
+            permuteUntil(A,left+1,right, result);
+            swap(A,left,i);
+        }
+    }
+    void swap(ArrayList<Integer> A, int l, int r){
+        int temp = A.get(l);
+        A.set(l,A.get(r));
+        A.set(r,temp);
     }
 
 
