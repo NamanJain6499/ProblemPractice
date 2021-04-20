ce74f7109571ef0d239730d61821c88c79f5b40b
NamanJain6499 <na.jain660@gmail.com>
2021-04-21 00:42:32 +0530

    first commit

--- src/InterviewBitPractice/Tree/KthSmallestElementInTree.java
+++ src/InterviewBitPractice/Tree/KthSmallestElementInTree.java
@@ -1,7 +1,79 @@
 package InterviewBitPractice.Tree;
 
-public class KthSmallestElementInTree {
-    public static void main(String args[]){
+//refer this blog on how to create a tree
+//https://www.baeldung.com/java-binary-tree
 
+
+//we know the fact that INORDER is already in sorted form and we make count varible which will count Kth min value
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
+class BST {
+
+    public TreeNode insert(TreeNode node, int data) {
+        if (node == null) {
+            return new TreeNode(data);
+        }
+
+        if (data < node.val) {
+            node.left = insert(node.left, data);
+        } else if ((data > node.val)) {
+            node.right = insert(node.right, data);
+        }
+
+        return node;
+    }
+
+
+}
+
+public class KthSmallestElementInTree {
+
+    public static void main(String[] args) {
+        KthSmallestElementInTree smallestElementInTree = new KthSmallestElementInTree();
+        BST a = new BST();
+        TreeNode root = null;
+
+        root = a.insert(root, 20);
+        root = a.insert(root, 8);
+        root = a.insert(root, 22);
+        root = a.insert(root, 4);
+        root = a.insert(root, 12);
+        root = a.insert(root, 10);
+        root = a.insert(root, 14);
+        int answer = smallestElementInTree.kthSmallest(root, 3);
+        System.out.println(answer);
+
+    }
+
+    int count = 0;
+    int out = 0;
+
+    public int kthSmallest(TreeNode root, int k) {
+        recur(root, k);
+        return out;
+    }
+
+    public void recur(TreeNode node, int k) {
+        if (node.left != null) {
+            recur(node.left, k);
+        }
+        count++;
+        if (count == k) {
+            out = node.val;
+            return;
+        }
+        if (node.right != null) {
+            recur(node.right, k);
+        }
     }
 }
--- src/InterviewBitPractice/Tree/ValidBstfromPreorder.java
+++ src/InterviewBitPractice/Tree/ValidBstfromPreorder.java
@@ -4,31 +4,34 @@ package InterviewBitPractice.Tree;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
+
 //complete solution
 //https://www.youtube.com/watch?v=GYdC4hQSo8A
 public class ValidBstfromPreorder {
-    public static void main(String []args){
-        ValidBstfromPreorder preorder=new ValidBstfromPreorder();
-        Scanner scanner=new Scanner(System.in);
-        ArrayList<Integer> A=new ArrayList<Integer>();
-        System.out.println("Enter elementsw of preorder traversal:");
-        while (scanner.hasNextInt()){
+    public static void main(String[] args) {
+        ValidBstfromPreorder preorder = new ValidBstfromPreorder();
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> A = new ArrayList<Integer>();
+        System.out.println("Enter elements of preorder traversal:");
+        while (scanner.hasNextInt()) {
             A.add(scanner.nextInt());
         }
         System.out.println((int) solve(A));
     }
-//we take first input of arraylist and find first number in A greater than input and check that all element on the rightof that should be greater than input
+
+    //we take first input of arraylist and find first number in A greater than input and check that all element on the rightof that should be greater than input
     private static int solve(ArrayList<Integer> A) {
-        Stack<Integer> stack=new Stack<>();
-        int root=Integer.MIN_VALUE;
-        for (int i=0;i<A.size();i++){
-            while (!stack.isEmpty()&&A.get(i)>stack.peek()){
-                root=stack.peek();
+        Stack<Integer> stack = new Stack<>();
+        int root = Integer.MIN_VALUE;
+        for (int i = 0; i < A.size(); i++) {
+            while (!stack.isEmpty() && A.get(i) > stack.peek()) {
+                root = stack.peek();
                 stack.pop();
             }
-            if (A.get(i)<root){
+            if (A.get(i) < root) {
                 return 0;
-            }stack.push(A.get(i));
+            }
+            stack.push(A.get(i));
         }
         return 1;
     }
