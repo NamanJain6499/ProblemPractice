9125d70dbf88aad16e17db3d17d74d143a82d0b8
NamanJain6499 <na.jain660@gmail.com>
2021-04-29 00:45:55 +0530

    first commit

--- src/InterviewBitPractice/ArrayList/LeadersInArray.java
+++ src/InterviewBitPractice/ArrayList/LeadersInArray.java
@@ -1,4 +1,30 @@
 package InterviewBitPractice.ArrayList;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class LeadersInArray {
+    public static void main(String args[]){
+        LeadersInArray leader=new LeadersInArray();
+        Scanner scanner=new Scanner(System.in);
+        ArrayList<Integer> A=new ArrayList<>();
+        while (scanner.hasNextInt()){
+            A.add(scanner.nextInt());
+        }
+        ArrayList<Integer> answer=leader.solve(A);
+        System.out.println(answer);
+
+    }
+    public ArrayList<Integer> solve(ArrayList<Integer> A){
+        int n=A.size();
+        int max_element=0;
+        ArrayList<Integer> res=new ArrayList<>();
+        for (int i=n-1;i>=0;i--){
+            if (A.get(i)>max_element){
+                max_element=A.get(i);
+                res.add(A.get(i));
+            }
+        }
+        return res;
+    }
 }
